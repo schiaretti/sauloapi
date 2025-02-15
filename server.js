9f21d736ddea5cf0b76fcaaebad4dccac5498ec5
ronischiaretti <pnamaquina@gmail.com>
2025-02-15 14:25:03 -0300

    teste

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import express from "express"
 import publicRoutes from './routes/public.js'
+import cors from 'cors'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use('/', publicRoutes)
 
