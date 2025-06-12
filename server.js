2aea23235ff04bed55817913560181ab1babbeef
ronischiaretti <pnamaquina@gmail.com>
2025-06-12 11:11:10 -0300

    1206002

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import routes from './routes/public.js'; // Ajuste o caminho conforme necessário
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
